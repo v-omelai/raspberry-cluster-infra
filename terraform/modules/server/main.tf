e82099bcbcf5cb9d47fc4ed4d54e9dee72c5ba65
Vitalii Omelai <v.omelay@gmail.com>
2025-02-27 18:47:26 +0200

    Added HDD mount

--- terraform/modules/server/main.tf
+++ terraform/modules/server/main.tf
@@ -17,6 +17,20 @@ resource "null_resource" "server" {
   provisioner "remote-exec" {
     inline = [
       "echo 'Running on ${self.triggers.host}'",
+
+      "sudo mkdir -p /var/lib/longhorn",
+      "if mountpoint -q /var/lib/longhorn; then sudo umount -l /var/lib/longhorn; fi",
+      "if ! sudo blkid /dev/sda | grep -q 'TYPE=\"ext4\"'; then",
+      "  sudo wipefs --all --force /dev/sda",
+      "  sudo mkfs.ext4 /dev/sda",
+      "fi",
+      "sudo mount /dev/sda /var/lib/longhorn",
+      "sudo chmod 777 /var/lib/longhorn",
+      "if ! grep -q '^/dev/sda /var/lib/longhorn ext4 ' /etc/fstab; then",
+      "  echo '/dev/sda /var/lib/longhorn ext4 defaults 0 0' | sudo tee -a /etc/fstab",
+      "fi",
+      "sudo systemctl daemon-reload",
+
       "sudo ip -4 route get 1.1.1.1 | grep -oP 'src \\K\\S+' > /tmp/.server/address",
       "curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 sh -s - --bind-address $(cat /tmp/.server/address)",
       "sleep ${self.triggers.sleep}",
@@ -24,6 +38,7 @@ resource "null_resource" "server" {
       "sudo cat /etc/rancher/k3s/k3s.yaml > /tmp/.server/config",
       "grep -qxF 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' ~/.bashrc || echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc",
       "source ~/.bashrc",
+
       "echo 'K3s server is now ready'",
     ]
   }
@@ -35,6 +50,10 @@ resource "null_resource" "server" {
   provisioner "remote-exec" {
     when = destroy
     inline = [
+      "if mountpoint -q /var/lib/longhorn; then sudo umount -l /var/lib/longhorn; fi",
+      "sudo rm -rf /var/lib/longhorn",
+      "sudo sed -i '/\\/var\\/lib\\/longhorn/d' /etc/fstab",
+      "sudo systemctl daemon-reload",
       "sudo k3s-killall.sh",
       "sudo k3s-uninstall.sh",
     ]
